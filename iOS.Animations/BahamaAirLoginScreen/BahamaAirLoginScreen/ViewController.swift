0421efd7724c2c2c04598bcb800f013104a9ff5c
MWLBear <771646799@qq.com>
2021-05-08 18:16:51 +0800

    add some_springs_animation

--- iOS.Animations/BahamaAirLoginScreen/BahamaAirLoginScreen/ViewController.swift
+++ iOS.Animations/BahamaAirLoginScreen/BahamaAirLoginScreen/ViewController.swift
@@ -85,6 +85,9 @@ class ViewController: UIViewController {
         cloud2.alpha = 0
         cloud3.alpha = 0
         cloud4.alpha = 0
+        
+        loginButton.center.y += 30
+        loginButton.alpha = 0
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -93,37 +96,51 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 0.5) {
             self.heading.center.x += self.view.bounds.width
         }
-        UIView.animate(withDuration: 0.5, delay: 0.3, options: [], animations: {
+
+        UIView.animate(withDuration: 0.5, delay: 0.3, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: [], animations: {
             self.username.center.x += self.view.bounds.width
         }, completion: nil)
         
-        UIView.animate(withDuration: 0.5, delay: 0.4, options: [], animations: {
+        UIView.animate(withDuration: 0.5, delay: 0.4, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: [], animations: {
             self.password.center.x += self.view.bounds.width
         }, completion: nil)
+        
         
         UIView.animate(withDuration: 0.5, delay: 0.5, options: [], animations: {
             self.cloud1.alpha = 1
         }, completion: nil)
-        
         UIView.animate(withDuration: 0.5, delay: 0.7, options: [], animations: {
             self.cloud2.alpha = 1
         }, completion: nil)
-        
         UIView.animate(withDuration: 0.5, delay: 0.9, options: [], animations: {
             self.cloud3.alpha = 1
         }, completion: nil)
-       
         UIView.animate(withDuration: 0.5, delay: 1.1, options: [], animations: {
             self.cloud4.alpha = 1
         }, completion: nil)
         
-      
+        UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: [], animations: {
+            self.loginButton.center.y -= 30
+            self.loginButton.alpha = 1
+        }, completion: nil)
+        
     }
     
     // MARK: further methods
     
     @IBAction func login() {
         view.endEditing(true)
+        
+        UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0, options: [], animations: {
+            self.loginButton.bounds.size.width += 80.0
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.33, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: [], animations: {
+            self.loginButton.center.y += 60
+            self.loginButton.backgroundColor = UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0)
+            self.spinner.center = CGPoint(x: 40, y: self.loginButton.frame.size.height / 2)
+            self.spinner.alpha = 1
+        }, completion: nil)
     }
     
     // MARK: UITextFieldDelegate
