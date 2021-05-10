a9f8ad7f1b94808ff7c9fece79d193ff90915825
MWLBear <771646799@qq.com>
2021-05-10 10:11:16 +0800

    add transtion animation

--- iOS.Animations/BahamaAirLoginScreen/BahamaAirLoginScreen/ViewController.swift
+++ iOS.Animations/BahamaAirLoginScreen/BahamaAirLoginScreen/ViewController.swift
@@ -51,6 +51,32 @@ class ViewController: UIViewController {
     var statusPosition = CGPoint.zero
     
     // MARK: view controller methods
+    func showMessage(index: Int) {
+        label.text = messages[index]
+        UIView.transition(with: status, duration: 0.33, options: [.curveEaseOut,.transitionCurlDown]) {
+            self.status.isHidden = false
+            self.status.center = self.statusPosition
+        } completion: { _ in
+            delay(2.0) {
+                if index < self.messages.count - 1 {
+                    self.removeMessage(index: index)
+                }
+            }
+        }
+    }
+    
+    func removeMessage(index: Int)  {
+        UIView.animate(withDuration: 0.33, delay: 0, options: []) {
+            self.status.center.x += self.view.frame.size.width
+        } completion: { _ in
+            self.status.isHidden = true
+            self.status.center = self.statusPosition
+            
+            self.showMessage(index: index + 1)
+        }
+
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,6 +99,8 @@ class ViewController: UIViewController {
         label.textColor = UIColor(red: 0.89, green: 0.38, blue: 0.0, alpha: 1.0)
         label.textAlignment = .center
         status.addSubview(label)
+        
+        statusPosition = status.center
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -133,7 +161,9 @@ class ViewController: UIViewController {
         
         UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0, options: [], animations: {
             self.loginButton.bounds.size.width += 80.0
-        }, completion: nil)
+        }) { _ in
+            self.showMessage(index: 0)
+        }
         
         UIView.animate(withDuration: 0.33, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: [], animations: {
             self.loginButton.center.y += 60
