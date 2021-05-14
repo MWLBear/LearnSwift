cd8b7ba6ab5db6da13dc7b06b76aabb8590f58bf
MWLBear <771646799@qq.com>
2021-05-14 16:21:44 +0800

    update MultiplayerSearch

--- iOS.Animations/MultiplayerSearch/MultiplayerSearch/AvatarView.swift
+++ iOS.Animations/MultiplayerSearch/MultiplayerSearch/AvatarView.swift
@@ -4,74 +4,118 @@ import QuartzCore
 
 @IBDesignable
 class AvatarView: UIView {
-  
-  //constants
-  let lineWidth: CGFloat = 6.0
-  let animationDuration = 1.0
-  
-  //ui
-  let photoLayer = CALayer()
-  let circleLayer = CAShapeLayer()
-  let maskLayer = CAShapeLayer()
-  let label: UILabel = {
-    let label = UILabel()
-    label.font = UIFont(name: "ArialRoundedMTBold", size: 18.0)
-    label.textAlignment = .center
-    label.textColor = UIColor.black
-    return label
-  }()
-  
-  //variables
-  @IBInspectable
-  var image: UIImage? = nil {
-    didSet {
-      photoLayer.contents = image?.cgImage
-    }
-  }
-  
-  @IBInspectable
-  var name: String? = nil {
-    didSet {
-      label.text = name
-    }
-  }
-  
-  var shouldTransitionToFinishedState = false
-  
-  override func didMoveToWindow() {
-    photoLayer.backgroundColor = UIColor.red.cgColor
-    layer.addSublayer(photoLayer)
-   // photoLayer.mask = maskLayer
-   // layer.addSublayer(circleLayer)
-    addSubview(label)
-  }
-  
-  override func layoutSubviews() {
-    super.layoutSubviews()
     
-    guard let image = image else {
-      return
+    //constants
+    let lineWidth: CGFloat = 6.0
+    let animationDuration = 1.0
+    
+    //ui
+    let photoLayer = CALayer()
+    let circleLayer = CAShapeLayer()
+    let maskLayer = CAShapeLayer()
+    let label: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "ArialRoundedMTBold", size: 18.0)
+        label.textAlignment = .center
+        label.textColor = UIColor.black
+        return label
+    }()
+    
+    //variables
+    @IBInspectable
+    var image: UIImage? = nil {
+        didSet {
+            photoLayer.contents = image?.cgImage
+        }
     }
     
-    //Size the avatar image to fit
-    photoLayer.frame = CGRect(
-      x: (bounds.size.width - image.size.width + lineWidth)/2,
-      y: (bounds.size.height - image.size.height - lineWidth)/2,
-      width: image.size.width,
-      height: image.size.height)
+    @IBInspectable
+    var name: String? = nil {
+        didSet {
+            label.text = name
+        }
+    }
     
-    //Draw the circle
-    circleLayer.path = UIBezierPath(ovalIn: bounds).cgPath
-    circleLayer.strokeColor = UIColor.white.cgColor
-    circleLayer.lineWidth = lineWidth
-    circleLayer.fillColor = UIColor.clear.cgColor
+    var shouldTransitionToFinishedState = false
+    var isSquare = false
     
-    //Size the layer
-    maskLayer.path = circleLayer.path
-    maskLayer.position = CGPoint(x: 0.0, y: 20)
+    override func didMoveToWindow() {
+        layer.addSublayer(photoLayer)
+        photoLayer.mask = maskLayer
+        layer.addSublayer(circleLayer)
+        addSubview(label)
+    }
     
-    //Size the label
-    label.frame = CGRect(x: 0.0, y: bounds.size.height + 10.0, width: bounds.size.width, height: 24.0)
-  }
-  
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        guard let image = image else {
+            return
+        }
+        
+        //Size the avatar image to fit
+        photoLayer.frame = CGRect(
+            x: (bounds.size.width - image.size.width + lineWidth)/2,
+            y: (bounds.size.height - image.size.height - lineWidth)/2,
+            width: image.size.width,
+            height: image.size.height)
+        
+        //Draw the circle
+        circleLayer.path = UIBezierPath(ovalIn: bounds).cgPath
+        circleLayer.strokeColor = UIColor.white.cgColor
+        circleLayer.lineWidth = lineWidth
+        circleLayer.fillColor = UIColor.clear.cgColor
+        
+        //Size the layer
+        maskLayer.path = circleLayer.path
+        maskLayer.position = CGPoint(x: 0.0, y: 10)
+        
+        //Size the label
+        label.frame = CGRect(x: 0.0, y: bounds.size.height + 10.0, width: bounds.size.width, height: 24.0)
+    }
+    
+    func bounceOff(point: CGPoint, morphSize: CGSize) {
+        let originalCenter = center
+        UIView.animate(withDuration: animationDuration, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.0) {
+            self.center = point
+        } completion: { _ in
+            if self.shouldTransitionToFinishedState {
+                print("shouldTransitionToFinishedState")
+               self.animateToSquare()
+            }
+        }
+
+        UIView.animate(withDuration: animationDuration, delay: animationDuration, usingSpringWithDamping: 0.7, initialSpringVelocity: 1.0, options: []) {
+            self.center = originalCenter
+        } completion: { _ in
+            delay(seconds: 0.1) {
+                if !self.isSquare {
+                    self.bounceOff(point: point, morphSize: morphSize)
+                }
+            }
+        }
+        let morphedFrame = (originalCenter.x > point.x) ?
+            CGRect(x: 0.0, y: bounds.height - morphSize.height, width: morphSize.width, height: morphSize.height) :
+            CGRect(x: bounds.width - morphSize.width, y: bounds.height - morphSize.height, width: morphSize.width, height: morphSize.height)
+        let morphAnimation = CABasicAnimation(keyPath: "path")
+        morphAnimation.duration = animationDuration
+        morphAnimation.toValue = UIBezierPath(ovalIn: morphedFrame).cgPath
+        morphAnimation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        circleLayer.add(morphAnimation, forKey: nil)
+        maskLayer.add(morphAnimation, forKey: nil)
+    }
+    
+    func animateToSquare(){
+        isSquare = true
+        let squarePath = UIBezierPath(rect: bounds).cgPath
+        let squareAnimation = CABasicAnimation(keyPath: "path")
+        squareAnimation.duration = 0.25
+        squareAnimation.fromValue = circleLayer.path
+        squareAnimation.toValue = squarePath
+        circleLayer.add(squareAnimation, forKey: nil)
+        maskLayer.add(squareAnimation, forKey: nil)
+       
+        circleLayer.path = squarePath
+        maskLayer.path = squarePath
+    }
 }
--- iOS.Animations/MultiplayerSearch/MultiplayerSearch/ViewController.swift
+++ iOS.Animations/MultiplayerSearch/MultiplayerSearch/ViewController.swift
@@ -3,25 +3,58 @@ import UIKit
 
 // A delay function
 func delay(seconds: Double, completion: @escaping ()-> Void) {
-  DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
+    DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
 }
 
 class ViewController: UIViewController {
-  
-  @IBOutlet var myAvatar: AvatarView!
-  @IBOutlet var opponentAvatar: AvatarView!
-  
-  @IBOutlet var status: UILabel!
-  @IBOutlet var vs: UILabel!
-  @IBOutlet var searchAgain: UIButton!
-  
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
     
-  }
-  
-  @IBAction func actionSearchAgain() {
-    UIApplication.shared.keyWindow!.rootViewController = storyboard!.instantiateViewController(withIdentifier: "ViewController") as UIViewController
-  }
+    @IBOutlet var myAvatar: AvatarView!
+    @IBOutlet var opponentAvatar: AvatarView!
+    
+    @IBOutlet var status: UILabel!
+    @IBOutlet var vs: UILabel!
+    @IBOutlet var searchAgain: UIButton!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        searchForOpponent()
+    }
+    
+    @IBAction func actionSearchAgain() {
+        UIApplication.shared.keyWindow!.rootViewController = storyboard!.instantiateViewController(withIdentifier: "ViewController") as UIViewController
+    }
+    
+    func searchForOpponent(){
+        let avatarSize = myAvatar.frame.size
+        let boundXOffset : CGFloat = avatarSize.width/1.9
+        let morphSize = CGSize(width: avatarSize.width*0.85, height: avatarSize.height*1.1)
+       
+        let rightBouncePoint = CGPoint(x: view.frame.size.width / 2 + boundXOffset, y: myAvatar.center.y)
+        let leftBouncePoint = CGPoint(x: view.frame.size.width / 2 - boundXOffset, y: opponentAvatar.center.y)
+        
+        myAvatar.bounceOff(point: rightBouncePoint, morphSize: morphSize)
+        opponentAvatar.bounceOff(point: leftBouncePoint, morphSize: morphSize)
+        
+        delay(seconds: 4.0, completion: foundOpponent)
+    }
+ 
+    func foundOpponent() {
+        status.text = "Connecting..."
+        opponentAvatar.image = UIImage(named: "avatar-2")
+        opponentAvatar.name = "Ray"
+        delay(seconds: 4.0, completion: connectedtoOpponet)
+    }
+    func connectedtoOpponet() {
+        myAvatar.shouldTransitionToFinishedState = true
+        opponentAvatar.shouldTransitionToFinishedState = true
+        delay(seconds: 1.0, completion: completed)
+    }
+    func completed() {
+        status.text = "Ready to play"
+        UIView.animate(withDuration: 0.2) {
+            self.vs.alpha = 1.0
+            self.searchAgain.alpha = 1.0
+        }
+    }
 }
 
